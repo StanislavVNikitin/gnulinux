d5c18db64da381bdbdb4686ec3e5b3322853fd3c
stas <stasvnikitin@mail.ru>
2023-10-12 13:14:06 +0300

    Add Model Profile, Page, Post, Category, Tag

--- gnulinux/admin.py
+++ gnulinux/admin.py
@@ -1,3 +1,111 @@
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django.contrib import admin
+from django import forms
+from django.utils.safestring import mark_safe
+
+from .models import *
+
 
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    save_as = True
+    save_on_top = True
+    list_display = ("id", "user", "get_avatar")
+    list_display_links = ("id", "user")
+    search_fields = ("user",)
+    readonly_fields = ("get_avatar",)
+    fields = ("id", "user", "get_avatar", "birthday")
+
+    def get_avatar(self, obj):
+        if obj.avatar:
+            return mark_safe(f'<img src="{obj.avatar.url}" width="50">')
+        return "-"
+
+    get_avatar.short_description = "Аватар"
+
+class PageAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Page
+        fields = "__all__"
+
+
+class PageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    form = PageAdminForm
+    save_as = True
+    save_on_top = True
+    list_display = ("id", "title", "slug", "created_at")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+    readonly_fields = ("created_at",)
+    fields = ("title", "slug",  "content", "created_at")
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = "__all__"
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    form = PostAdminForm
+    save_as = True
+    save_on_top = True
+    list_display = ("id", "title", "slug", "category", "created_at", "get_photo")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+    list_filter = ("category",)
+    readonly_fields = ("views", "created_at", "get_photo")
+    fields = ("title", "slug", "user", "category", "tags", "content", "photo", "get_photo", "views", "created_at")
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return "-"
+
+    get_photo.short_description = "Фото"
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    save_as = True
+    save_on_top = True
+    list_display = ("id", "title", "slug", "get_photo")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+    readonly_fields = ("get_photo",)
+    fields = ("title", "slug", "photo", "get_photo")
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return "-"
+
+    get_photo.short_description = "Фото"
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    save_as = True
+    save_on_top = True
+    list_display = ("id", "title", "slug")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+    fields = ("title", "slug")
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+
+
+admin.site.site_title = 'Управление сайтом GnuLinux'
+admin.site.site_header = 'Управление сайтом GnuLinux'
--- gnulinux/models.py
+++ gnulinux/models.py
@@ -1,3 +1,142 @@
+import datetime
 from django.db import models
 
+from django.contrib.auth.models import User
+from PIL import Image
+from django.urls import reverse
+
 # Create your models here.
+"""
+Model Profile
+"""
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    birthday = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 120 or img.width > 120:
+            output_size = (120, 120)
+            img.thumbnail(output_size)
+            img.save(self.avatar.path)
+
+    def get_age(self):
+        age = datetime.date.today() - self.birthday
+        return int((age).days / 365.25)
+
+
+"""
+Model Category
+"""
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название")
+    slug = models.SlugField(max_length=255, verbose_name="Url категории", unique=True)
+    photo = models.ImageField(upload_to="photo/category/", blank=True, verbose_name="Картинка")
+    class_name = models.CharField(max_length=100, verbose_name="Класс CSS")
+    deleted = models.BooleanField(default=False, verbose_name='Удалено')
+
+    def __str__(self):
+        return self.title
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("gnulinux:category", kwargs={"slug": self.slug})
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["title"]
+
+
+"""
+Model Tag
+"""
+class Tag(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название")
+    slug = models.SlugField(max_length=100, verbose_name="Url тэга", unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("gnulinux:tag", kwargs={"slug": self.slug})
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+    class Meta:
+        verbose_name = "Тэг"
+        verbose_name_plural = "Тэги"
+        ordering = ["title"]
+
+
+"""
+Model Post
+"""
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название новости")
+    slug = models.SlugField(max_length=255, verbose_name="Url новости", unique=True)
+    content = models.TextField(blank=True, verbose_name="Текст новости")
+    photo = models.ImageField(upload_to="photo/%Y/%m/%d/", blank=True, verbose_name="Картинка")
+    category = models.ForeignKey("Category", blank=True, on_delete=models.PROTECT, related_name="posts",
+                                 verbose_name="Категория")
+    tags = models.ManyToManyField("Tag", blank=True, related_name="posts", verbose_name="Тэг")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts",
+                             verbose_name="Пользователь")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    views = models.IntegerField(default=0, verbose_name="Количество просмотров")
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    deleted = models.BooleanField(default=False, verbose_name='Удалено')
+
+    def __str__(self):
+        return self.title
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("gnulinux:post", kwargs={"slug": self.slug})
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+        ordering = ["-created_at"]
+
+"""
+Model Page
+"""
+class Page(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название страницы")
+    slug = models.SlugField(max_length=255, verbose_name="Url страницы", unique=True)
+    content = models.TextField(blank=True, verbose_name="Текст страницы")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата публикации")
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    deleted = models.BooleanField(default=False, verbose_name='Удалено')
+
+    def __str__(self):
+        return self.title
+
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("gnulinux:page", kwargs={"slug": self.slug})
+
+    class Meta:
+        verbose_name = "Страница"
+        verbose_name_plural = "Страницы"
+        ordering = ["-created_at"]
